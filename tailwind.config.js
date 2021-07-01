ccdfc894e19ca63def50445abef6c35af75905d6
Dusan <dusancajic89@gmail.com>
2021-07-01 10:34:15 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: ["./dist/**/*.html"],
+  theme: {
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1600px',
+    },
+    extend: {
+      fontFamily: {
+        //sans: ['Roboto', 'sans-serif'],
+        //serif: ['Fake Serif', 'serif'],
+      },
+      container: {
+        padding: '15px',
+        center: true,
+      },
+      colors: {
+        //primary: '#aa2249',
+        //secondary: '#771833',
+      },
+      backgroundImage: theme => ({
+        'splash-pattern': "url('https://media.cmsmax.com/fqsmwuwheeyba5isyhksr/transparent-bg.png')",
+       })
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
